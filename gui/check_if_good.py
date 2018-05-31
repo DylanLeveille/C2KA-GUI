dc283d74aedb60b8948b33259833f5c7b6249674
Dylan Leveille <39699701+AppetiteForDestruction@users.noreply.github.com>
2018-05-31 10:06:24 -0400

    Update check_if_good.py

--- gui/check_if_good.py
+++ gui/check_if_good.py
@@ -1,5 +1,69 @@
 from tkinter import*
 
+#
+#Functions which validate entries/create pop-ups:
+#
+
+#function is called when an incorrect stimuli is entered
+def incorrect_stims(main):
+  global wrongStims
+  #create pop-up window
+  wrongStims = Toplevel()
+  windowSize = 300
+  screenWidth = wrongStims.winfo_screenwidth() 
+  screenHeight = wrongStims.winfo_screenheight()
+  positionRight = screenWidth/2 - windowSize/2
+  positionDown = screenHeight/2 - windowSize/2
+  
+  wrongStims.geometry('%dx%d+%d+%d' % (windowSize, windowSize, 
+                                    positionRight, positionDown)) 
+  
+  wrongStims.resizable(width = False, height = False) 
+
+  wrongStims.wm_title("Incorrect stimuli")
+  wrongStims.overrideredirect(1)
+  Label(wrongStims, text = 'Please enter at least one valid stimulus').pack(side = TOP)
+  Label(wrongStims, text = 'or remove invalid stimuli').pack(side = TOP)
+  
+  pressToClose = Button(wrongStims, text = "Return", 
+                        command = lambda: return_to_stims(main))
+  pressToClose.pack(side = BOTTOM)
+  #remove main window  
+  main.withdraw()    
+
+#function is called when an incorrect behaviour is entered
+def incorrect_bevs(main):
+  global wrongBevs
+  #create pop-up window
+  wrongBevs = Toplevel()
+    
+  windowSize = 300
+  
+  screenWidth = wrongBevs.winfo_screenwidth() 
+  screenHeight = wrongBevs.winfo_screenheight()
+    
+  positionRight = screenWidth/2 - windowSize/2
+  positionDown = screenHeight/2 - windowSize/2
+    
+  wrongBevs.geometry('%dx%d+%d+%d' % (windowSize, windowSize, 
+                                      positionRight, positionDown)) 
+    
+  wrongBevs.resizable(width = False, height = False) 
+
+  wrongBevs.wm_title("Incorrect behaviour(s)")
+  wrongBevs.overrideredirect(1)
+    
+  Label(wrongBevs, text = 'Please enter at least one valid behaviour').pack(side = TOP)
+  Label(wrongBevs, text = 'or remove invalid behaviours').pack(side = TOP)
+    
+  pressToClose = Button(wrongBevs, text = "Return", 
+                        command = lambda: return_to_bevs(main))
+  pressToClose.pack(side = BOTTOM)
+  
+  #remove main window  
+  main.withdraw()    
+
+#function is callled to verify entries in tables
 def check_if_good(bevDict, stimDict, circleTableBoxes, lambdaTableBoxes, 
                   circleTableValues, lambdaTableValues):
   invalidEntries = 0 #a counter for invalid entries
@@ -37,4 +101,24 @@ def check_if_good(bevDict, stimDict, circleTableBoxes, lambdaTableBoxes,
   if firstFlag == True and secondFlag == True:
     return True, invalidEntries
   else:
-    return False, invalidEntries
+    return False, invalidEntries 
+
+#
+#Functions which regenerate the main window
+#
+
+#function to return to the stimuli entry page after the pop-up 
+def return_to_stims(main):
+  main.update()
+  main.deiconify()
+  wrongStims.destroy() 
+ 
+ #regenerate the stim boxes that were un-packed
+ #for i in range(len(stimList)):   
+ # stimList[i].pack(side = TOP) 
+
+#function to return to the behaviour entry page after the pop-up 
+def return_to_bevs(main):
+  main.update()
+  main.deiconify()
+  wrongBevs.destroy() 
