3eccd3ecb7a6dcf11821bf53affb06a35c49f879
idirz <idirzerrouk11@gmail.com>
2018-06-05 10:24:08 -0400

    Add files via upload

---
+++ gui/gui/CBS.py
@@ -0,0 +1,35 @@
+from tkinter import*
+from delete_CBS import *
+
+
+def create_CBS_entries(bevDict, FrameCBS, rowNum):
+    Entries = {}
+    Labels = {}
+    for i in range(1, len(bevDict)+1):
+        LabelCBS = Label(FrameCBS, text = bevDict[i])
+        EntryCBS = Entry(FrameCBS)
+        LabelCBS.pack(anchor = W)
+        EntryCBS.pack(anchor = W)
+        Labels[i] = LabelCBS
+        Entries[i] = EntryCBS
+        rowNum+=1
+
+    return Entries, Labels, rowNum
+
+def fix_CBS(bevDict, Entries, Labels, FrameCBS, rowNum):
+    currRow = 1
+    count = 0
+    count = delete_CBS(rowNum, currRow, count, bevDict, Entries, Labels)
+    newRowNum = rowNum - count
+    while count>0:
+        
+        del Labels[rowNum - count+1]
+        del Entries[rowNum - count+1]
+        count-=1
+    
+    return newRowNum
+    
+
+    
+        
+
