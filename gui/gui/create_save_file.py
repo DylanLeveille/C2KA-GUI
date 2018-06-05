ff4b939daf2163e3f91e86907a66f7ac5b42a3dc
Dylan Leveille <dylan.leveille321@gmail.com>
2018-06-05 16:06:17 -0400

    Add files via upload

---
+++ gui/gui/create_save_file.py
@@ -0,0 +1,11 @@
+from tkinter import filedialog
+
+def create_save_file():
+    pathName =  filedialog.asksaveasfilename(initialdir = "/", title = "Save text file", filetypes = ((".txt","*.txt"), ("all files","*.*")), initialfile = 'agentspec.txt')
+ 
+    dst = open(pathName, "w+")
+    src = open("agentspec.txt", "r")
+    dst.write(src.read())
+
+    dst.close()
+    src.close()
