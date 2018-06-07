e06989de0a23aafb73bcc332d551cc09f8b5d2c8
Dylan Leveille <dylan.leveille321@gmail.com>
2018-06-07 15:56:43 -0400

    Add files via upload

--- gui/gui/CBS_mods.py
+++ gui/gui/CBS_mods.py
@@ -1,87 +1,111 @@
-from tkinter import*
-from delete_CBS import *
-from add_CBS import*
+"""Imported modules."""
+from tkinter import * ##Import the tkinter module to allow construction of the GUI interface.
+from delete_CBS import * ##Contains a function which deletes concrete behaviour rows. 
+from add_CBS import * ##Contains a function which adds concrete behaviour rows.
 
-#function called when no CBS were yet generated
+"""Functions which will adapt to any changes brought to the concrete behaviours."""
+##Function called when no CBS were yet generated.
 def create_CBS_entries(bevDict, frameCBS):
-    #create an empty dictionary to hold entries on concrete behaviours page
-    entriesCBS = {}
+  """ (dict, tkinter.Frame) -> (dict)
     
-    #iterate across the behaviour dictionary to create concrete behaviours
-    #in the frameCBS
-    for i in range(1, len(bevDict) + 1):
-        #create label/entry
-        labelCBS = Label(frameCBS, text = bevDict[i])
-        entryCBS = Entry(frameCBS)
-        #pack label/entry
-        labelCBS.pack(anchor = W)
-        entryCBS.pack(anchor = W)
-        
-        #assign label/entry to dictionary
-        entriesCBS[i, 0] = labelCBS
-        entriesCBS[i, 1] = entryCBS
-    
-    #return data structure
-    return entriesCBS
+    Creates the first set of concrete behaviours on the
+    CBS page. The entries are saved 
+  
+  """    
+  ##Create an empty dictionary to hold entries on concrete behaviours page.
+  entriesCBS = {}
+  
+  ##Iterate across the behaviour dictionary to create concrete behaviours
+  ##in the frameCBS.
+  for i in range(1, len(bevDict) + 1):
+      ##Create label/entry.
+      labelCBS = Label(frameCBS, text = bevDict[i])
+      entryCBS = Entry(frameCBS)
+      
+      ##Pack label/entry.
+      labelCBS.pack(anchor = W)
+      entryCBS.pack(anchor = W)
+      
+      ##Assign label/entry to dictionary.
+      entriesCBS[i, 0] = labelCBS
+      entriesCBS[i, 1] = entryCBS
+  
+  ##Return data structure.
+  return entriesCBS
 
-#called to see if any modifications are necessary to the CBS data
+##Called to see if any modifications are necessary to the CBS data.
 def fix_CBS(bevDict, frameCBS, entriesCBS):
-    #extract current number of rows in CBS
-    numRows = entriesCBS['numRows']
+  """ (dict, tkinter.Frame, dict) -> (dict)
     
-    #keep track of the current row for recursion in delete_CBS
-    currRow = 1
-    
-    #number of rows to delete
-    delRows = 0
-    
-    #check to see how many rows need to be deleted
-    delRows = delete_CBS(bevDict, currRow, numRows, entriesCBS, delRows)
-    
-    #save new number of rows in the dictionary
-    entriesCBS['numRows'] = numRows - delRows    
-    
-    while delRows > 0:
-        #get rid of label
-        entriesCBS[numRows - delRows + 1, 0].destroy()
-        
-        #get rid of entry
-        entriesCBS[numRows - delRows + 1, 1].destroy()        
-        
-        #remove the label/entry from the dictionary
-        del entriesCBS[numRows - delRows + 1, 0]
-        del entriesCBS[numRows - delRows + 1, 1]
-        
-        #decrease number of rows to delete by 1
-        delRows -= 1
-    
-    #save new number of rows
-    numRows = entriesCBS['numRows']
-    
-    #check to see how many rows need to be added
-    add_CBS(bevDict, numRows, entriesCBS, frameCBS)
-    
-    #return data scructure of new CBS entries/labels
-    return entriesCBS
+    Modifies the concrete behaviour data based on
+    any modifications to the behavioursthat may have 
+    been made when returning to a previous page.
+  
+  """    
+  ##Extract current number of rows in CBS.
+  numRows = entriesCBS[0, 0]
+  
+  ##Keep track of the current row for recursion in delete_CBS.
+  currRow = 1
+  
+  ##Number of rows to delete.
+  delRows = 0
+  
+  ##Check to see how many rows need to be deleted.
+  delRows = delete_CBS(bevDict, currRow, numRows, entriesCBS, delRows)
+  
+  ##Save new number of rows in the dictionary.
+  entriesCBS[0, 0] = numRows - delRows    
+  
+  while delRows > 0:
+      ##Get rid of label.
+      entriesCBS[numRows - delRows + 1, 0].destroy()
+      
+      ##Get rid of entry.
+      entriesCBS[numRows - delRows + 1, 1].destroy()        
+      
+      ##Remove the label/entry from the dictionary.
+      del entriesCBS[numRows - delRows + 1, 0]
+      del entriesCBS[numRows - delRows + 1, 1]
+      
+      ##Decrease number of rows to delete by 1.
+      delRows -= 1
+  
+  ##Save new number of rows.
+  numRows = entriesCBS[0, 0]
+  
+  ##Check to see how many rows need to be added.
+  add_CBS(bevDict, numRows, entriesCBS, frameCBS)
+  
+  ##Return data scructure of new CBS entries/labels.
+  return entriesCBS
 
+##Function that recreates the concrete behaviours on the page.
 def recreate_CBS_entries(bevDict, entriesCBS, frameCBS):
-    #itearte through the rows to re-generate them
-    for row in range(1, len(bevDict) + 1):
-        
-        #create CBS label
-        labelCBS = Label(frameCBS, text = bevDict[row])
-        
-        #create CBS entry
-        entryCBS = Entry(frameCBS)
-        entryCBS.insert(0, entriesCBS[row, 1].get())
-        
-        #pack new label/entry
-        labelCBS.pack(anchor = W)
-        entryCBS.pack(anchor = W)
-        
-        #save label/entry in the data structure
-        entriesCBS[row, 0] = labelCBS
-        entriesCBS[row, 1] = entryCBS
+  """ (dict, tkinter.Frame, dict) -> (dict)
     
-    #return updated data structure    
-    return entriesCBS
+    Recreates the scrolling area and frame
+    for the concrete behaviours based on the
+    new data.
+  
+  """   
+  ##Iterate through the rows to re-generate them.
+  for row in range(1, len(bevDict) + 1):
+      
+      ##Create CBS label.
+      labelCBS = Label(frameCBS, text = bevDict[row])
+      
+      ##Create CBS entry.
+      entryCBS = Entry(frameCBS)
+      entryCBS.insert(0, entriesCBS[row, 1].get())
+      
+      ##Pack new label/entry.
+      labelCBS.pack(anchor = W)
+      entryCBS.pack(anchor = W)
+      
+      ##Save label/entry in the data structure.
+      entriesCBS[row, 0] = labelCBS
+      entriesCBS[row, 1] = entryCBS
+  
+  ##Return updated data structure.    
+  return entriesCBS
--- gui/gui/check_if_good.py
+++ gui/gui/check_if_good.py
@@ -1,34 +1,42 @@
-from tkinter import*
-import vertSuperscroll 
+"""Imported modules."""
+from tkinter import * ##Import the tkinter module to allow construction of the GUI interface.
+import vertSuperscroll ##Module containing the widget allowing a vertical scrollbar.
 
-#
-#Functions which validate entries/create pop-ups:
-#
+"""Functions which validate entries/create pop-ups."""
 
-#function to warn user that stims will be deleted
+##Function to warn user that current stims will be deleted.
 def specify_stim(main, stimList, numStims, stimScrollingArea):
+  """ (tkinter.Tk, dict, int, tkinter.Frame) -> (none)
+    
+    Pop-up when the user specifies a number of
+    stimuli to add.
+  
+  """   
   global warningStims
-  #create pop-up window
-
+  ##Create pop-up window.
   warningStims = Toplevel()
 
-  warningStims.config(takefocus = True)  
+  warningStims.config(takefocus = True) ##Get focus on screen.  
   
-  windowSize = 300
+  windowSize = 300 ##Pop up size (300 x 300).
   
+  ##Collect screen (monitor) width and height to position the window in the center. 
   screenWidth = warningStims.winfo_screenwidth() 
   screenHeight = warningStims.winfo_screenheight()
   
+  ##Calculate the center position.
   positionRight = screenWidth/2 - windowSize/2
   positionDown = screenHeight/2 - windowSize/2
   
+  ##Set the window size using the geometry() method.
   warningStims.geometry('%dx%d+%d+%d' % (windowSize, windowSize, 
                                     positionRight, positionDown)) 
   
-  warningStims.resizable(width = False, height = False) 
+  warningStims.resizable(width = False, height = False) ##The window is not resizeable.
 
   warningStims.wm_title("WARNING")
-  warningStims.overrideredirect(1)
+  warningStims.overrideredirect(1) ##Removes task bar.
+  
   Label(warningStims, text = 'This action will permenantly delete the current stimuli').pack(side = TOP)
   
   pressToContinue = Button(warningStims, text = "Continue", 
@@ -40,95 +48,112 @@ def specify_stim(main, stimList, numStims, stimScrollingArea):
   pressToContinue.pack(side = BOTTOM)
   
   pressToClose.pack(side = BOTTOM)
-  #remove main window  
+  ##Removes main window.  
   main.withdraw()      
 
-#function to warn the user that the specified number of stims is invalid
+##Function to warn the user that the specified number of stims is invalid.
 def incorrect_stim_num(main):
+  """ (tkinter.Tk, dict, int, tkinter.Frame) -> (none)
+    
+    Pop-up when the user specifies a number of
+    stimuli to add.
+  
+  """     
   global wrongStimNum
-  #create pop-up window
+  ##create pop-up window
   wrongStimNum = Toplevel()
   
-  wrongStimNum.config(takefocus = True)  
+  wrongStimNum.config(takefocus = True) ##Get focus on screen.  
   
-  windowSize = 300
+  windowSize = 300 ##Pop up size (300 x 300).
   
+  ##Collect screen (monitor) width and height to position the window in the center. 
   screenWidth = wrongStimNum.winfo_screenwidth() 
   screenHeight = wrongStimNum.winfo_screenheight()
   
+  ##Calculate the center position.
   positionRight = screenWidth/2 - windowSize/2
   positionDown = screenHeight/2 - windowSize/2
   
+  ##Set the window size using the geometry() method.
   wrongStimNum.geometry('%dx%d+%d+%d' % (windowSize, windowSize, 
                                     positionRight, positionDown)) 
   
-  wrongStimNum.resizable(width = False, height = False) 
+  wrongStimNum.resizable(width = False, height = False) ##The window is not resizeable.
 
   wrongStimNum.wm_title("Incorrect stimuli number")
-  wrongStimNum.overrideredirect(1)
+  wrongStimNum.overrideredirect(1) ##Removes task bar.
+  
   Label(wrongStimNum, text = 'Please enter a valid number of stimuli').pack(side = TOP)
   
   pressToClose = Button(wrongStimNum, text = "Return", 
                         command = lambda: return_to_stim_num(main))
   pressToClose.pack(side = BOTTOM)
-  #remove main window
+  ##Removes main window.  
   main.withdraw()    
 
-#function is called when an incorrect stimuli is entered
+##Function is called when an incorrect stimuli is entered.
 def incorrect_stims(main):
   global wrongStims
-  #create pop-up window
+  ##Create pop-up window.
   wrongStims = Toplevel()
   
-  wrongStims.config(takefocus = True)  
+  wrongStims.config(takefocus = True) ##Get focus on screen. 
   
-  windowSize = 300
+  windowSize = 300 ##Pop up size (300 x 300).
   
+  ##Collect screen (monitor) width and height to position the window in the center. 
   screenWidth = wrongStims.winfo_screenwidth() 
   screenHeight = wrongStims.winfo_screenheight()
   
+  ##Calculate the center position.
   positionRight = screenWidth/2 - windowSize/2
   positionDown = screenHeight/2 - windowSize/2
   
+  ##Set the window size using the geometry() method.
   wrongStims.geometry('%dx%d+%d+%d' % (windowSize, windowSize, 
                                     positionRight, positionDown)) 
   
-  wrongStims.resizable(width = False, height = False) 
+  wrongStims.resizable(width = False, height = False) ##The window is not resizeable.
 
   wrongStims.wm_title("Incorrect stimuli")
-  wrongStims.overrideredirect(1)
+  wrongStims.overrideredirect(1) ##Removes task bar.
+  
   Label(wrongStims, text = 'Please enter at least one valid stimulus').pack(side = TOP)
   Label(wrongStims, text = 'or remove invalid stimuli').pack(side = TOP)
   
   pressToClose = Button(wrongStims, text = "Return", 
                         command = lambda: return_to_stims(main))
   pressToClose.pack(side = BOTTOM)
-  #remove main window  
+  ##Removes main window.  
   main.withdraw()    
 
-#function is called when an incorrect agent is entered
+##Function is called when an incorrect agent is entered.
 def incorrect_agent(main):
   global wrongAgent
-  #create pop-up window
+  ##Create pop-up window.
   wrongAgent = Toplevel()
     
-  wrongAgent.config(takefocus = True)  
+  wrongAgent.config(takefocus = True) ##Get focus on screen.   
     
-  windowSize = 300
+  windowSize = 300 ##Pop up size (300 x 300).
   
+  ##Collect screen (monitor) width and height to position the window in the center. 
   screenWidth = wrongAgent.winfo_screenwidth() 
   screenHeight = wrongAgent.winfo_screenheight()
-    
+  
+  ##Calculate the center position.  
   positionRight = screenWidth/2 - windowSize/2
   positionDown = screenHeight/2 - windowSize/2
-    
+  
+  ##Set the window size using the geometry() method.  
   wrongAgent.geometry('%dx%d+%d+%d' % (windowSize, windowSize, 
                                       positionRight, positionDown)) 
     
-  wrongAgent.resizable(width = False, height = False) 
+  wrongAgent.resizable(width = False, height = False) ##The window is not resizeable.
 
   wrongAgent.wm_title("Incorrect behaviour(s)")
-  wrongAgent.overrideredirect(1)
+  wrongAgent.overrideredirect(1) ##Removes task bar.
     
   Label(wrongAgent, text = 'Please enter one valid agent').pack(side = TOP)
     
@@ -136,33 +161,35 @@ def incorrect_agent(main):
                         command = lambda: return_to_bevs_agent(main))
   pressToClose.pack(side = BOTTOM)
   
-  #remove main window  
+  ##Removes main window.   
   main.withdraw()    
 
-
-#function is called when an incorrect behaviour is entered
+##Function is called when an incorrect behaviour is entered.
 def incorrect_bevs(main):
   global wrongBevs
-  #create pop-up window
+  ##Create pop-up window.
   wrongBevs = Toplevel()
   
-  wrongBevs.config(takefocus = True)
+  wrongBevs.config(takefocus = True) ##Get focus on screen. 
     
-  windowSize = 300
+  windowSize = 300 ##Pop up size (300 x 300).
   
+  ##Collect screen (monitor) width and height to position the window in the center. 
   screenWidth = wrongBevs.winfo_screenwidth() 
   screenHeight = wrongBevs.winfo_screenheight()
     
+  ##Calculate the center position.  
   positionRight = screenWidth/2 - windowSize/2
   positionDown = screenHeight/2 - windowSize/2
-    
+  
+  ##Set the window size using the geometry() method.  
   wrongBevs.geometry('%dx%d+%d+%d' % (windowSize, windowSize, 
                                       positionRight, positionDown)) 
     
-  wrongBevs.resizable(width = False, height = False) 
+  wrongBevs.resizable(width = False, height = False) ##The window is not resizeable.
 
   wrongBevs.wm_title("Incorrect behaviour(s)")
-  wrongBevs.overrideredirect(1)
+  wrongBevs.overrideredirect(1) ##Removes task bar.
     
   Label(wrongBevs, text = 'Please enter at least one valid behaviour').pack(side = TOP)
   Label(wrongBevs, text = 'or remove invalid behaviours').pack(side = TOP)
@@ -171,19 +198,19 @@ def incorrect_bevs(main):
                         command = lambda: return_to_bevs_bevs(main))
   pressToClose.pack(side = BOTTOM)
   
-  #remove main window  
+  ##Removes main window.  
   main.withdraw()    
 
-#function is called to verify entries in tables
-def check_if_good(bevDict, stimDict, circleTableBoxes, lambdaTableBoxes, 
+##Function is called to verify entries in tables.
+def check_if_good_table(bevDict, stimDict, circleTableBoxes, lambdaTableBoxes, 
                   circleTableValues, lambdaTableValues):
-  invalidEntries = 0 #a counter for invalid entries
+  invalidEntries = 0 ##A counter for invalid entries.
   
   firstFlag = True
   secondFlag = True
   
   found = False
-  #search for incorrect value in circleTableValues 
+  ##Search for incorrect value in circleTableValues. 
   for key in circleTableValues.keys():
     if circleTableValues[key] not in bevDict.values():
       if found == False:
@@ -191,12 +218,12 @@ def check_if_good(bevDict, stimDict, circleTableBoxes, lambdaTableBoxes,
         found = True
       circleTableBoxes[key].config(bg = 'red')
       invalidEntries += 1
-    #change background to white (normal) if the value is now valid  
+    ##Change background to white (normal) if the value is now valid.  
     else:
       circleTableBoxes[key].config(bg = 'white')    
   
   found = False    
-  #search for incorrect value in lambdaTableValues     
+  ##Search for incorrect value in lambdaTableValues.     
   for key in lambdaTableValues.keys():
     if lambdaTableValues[key] not in stimDict.values() and lambdaTableValues[key] != 'N' and lambdaTableValues[key] != 'D':
       if found == False:
@@ -204,11 +231,11 @@ def check_if_good(bevDict, stimDict, circleTableBoxes, lambdaTableBoxes,
         found = True
       lambdaTableBoxes[key].config(bg = 'red') 
       invalidEntries += 1
-    #change background to white (normal) if the value is now valid  
+    ##Change background to white (normal) if the value is now valid.  
     else: 
       lambdaTableBoxes[key].config(bg = 'white')
   
-  #if both tables contain valid values, return True; else return False    
+  ##If both tables contain valid values, return True; else return False.    
   if firstFlag == True and secondFlag == True:
     return True, invalidEntries
   else:
@@ -216,24 +243,28 @@ def check_if_good(bevDict, stimDict, circleTableBoxes, lambdaTableBoxes,
 
 def incorrect_table(main, numInvalid):
   global invalidEntryPop
-  #create pop-up window
+  ##Create pop-up window
   invalidEntryPop = Toplevel()
-  invalidEntryPop.config(takefocus = True)
+  invalidEntryPop.config(takefocus = True) ##Get focus on screen. 
   
-  windowSize = 300
+  windowSize = 300 ##Pop up size (300 x 300).
   
+  ##Collect screen (monitor) width and height to position the window in the center. 
   screenWidth = invalidEntryPop.winfo_screenwidth() 
   screenHeight = invalidEntryPop.winfo_screenheight()
   
+  ##Calculate the center position.
   positionRight = screenWidth/2 - windowSize/2
   positionDown = screenHeight/2 - windowSize/2
   
+  ##Set the window size using the geometry() method.
   invalidEntryPop.geometry('%dx%d+%d+%d' % (windowSize, windowSize, 
                                             positionRight, positionDown))    
-  invalidEntryPop.resizable(width = False, height = False) 
+  
+  invalidEntryPop.resizable(width = False, height = False) ##The window is not resizeable.
 
   invalidEntryPop.wm_title("INVALID ENTRIES!")
-  invalidEntryPop.overrideredirect(1)
+  invalidEntryPop.overrideredirect(1) ##Removes task bar.
   
   Label(invalidEntryPop, text = 'Invalid Entries:').pack(side = TOP)
   invalidEntriesLabel = Label(invalidEntryPop, text = str(numInvalid) + 
@@ -244,35 +275,33 @@ def incorrect_table(main, numInvalid):
                         command = lambda: return_to_tables(main))
   pressToClose.pack(side = BOTTOM)
   
-  #remove main window
+  ##Removes main window.  
   main.withdraw()  
 
-#
-#Functions which get rid of the pop-up window
-#
+"""Functions which get rid of the pop-up window."""
 
-#generate the specified stim entries 
+##Generate the specified stim entries. 
 def return_to_stims_deletion(main, stimList, numStims, stimScrollingArea):
   warningStims.destroy()
   
-  #test to see if more than one word in the entry box
-  #and test to see if it is a number only
+  ##Test to see if there is more than one word in the entry box
+  ##and test to see if it is a number only.
   if len(numStims.split()) > 1 or numStims.replace(' ', '').isdigit() != True: 
-    #call a function to pop up
+    ##Call a function to pop up.
     incorrect_stim_num(main) 
   
   else:
-    #extract the number from the string
+    ##Extract the number from the string.
     numStims = int(numStims.replace(' ', ''))  
     
-    #destroy the old frame 
+    ##Destroy the old frame. 
     stimScrollingArea[0].destroy()
 
-    #clear list for specified entries
+    ##Clear list for specified entries.
     stimList.clear()   
     
-    #make a new frame capable of scrolling to the new entry boxes specified
-    #by the user
+    ##Make a new frame capable of scrolling to the new entry boxes specified
+    ##by the user.
     stimScrollingArea[0] = vertSuperscroll.Scrolling_Area(main)
     stimScrollingArea[0].pack(expand = 1, fill=BOTH)
     
@@ -287,37 +316,37 @@ def return_to_stims_deletion(main, stimList, numStims, stimScrollingArea):
     main.update()
     main.deiconify()  
 
-#return to main when user wishes to cancel inputting the stimuli   
+##Function to return to main when user wishes to cancel inputting the stimuli.   
 def return_to_stims_cancellation(main):
   main.update()
   main.deiconify()
   warningStims.destroy()
 
-#return to main when invalid number is enetred for the stimuli  
+##Function to return to main when invalid number is enetred for the stimuli.  
 def return_to_stim_num(main):
   main.update()
   main.deiconify()
   wrongStimNum.destroy()  
 
-#function to return to the stimuli entry page after the pop-up 
+##Function to return to the stimuli entry page after the pop-up. 
 def return_to_stims(main):
   main.update()
   main.deiconify()
   wrongStims.destroy() 
 
-#function to return to the behaviour entry page after the pop-up 
+##Function to return to the behaviour entry page after the pop-up. 
 def return_to_bevs_agent(main):
   main.update()
   main.deiconify()
   wrongAgent.destroy() 
 
-#function to return to the behaviour entry page after the pop-up 
+##Function to return to the behaviour entry page after the pop-up. 
 def return_to_bevs_bevs(main):
   main.update()
   main.deiconify()
   wrongBevs.destroy() 
   
-#function to return to the tables after the pop-up 
+##Function to return to the tables after the pop-up. 
 def return_to_tables(main):
   main.update()
   main.deiconify()
