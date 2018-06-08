1e02f84e6f450775924a025c9e8ce806f4735e22
Dylan Leveille <dylan.leveille321@gmail.com>
2018-06-08 10:26:17 -0400

    Add files via upload

--- gui/gui/create_text.py
+++ gui/gui/create_text.py
@@ -1,70 +1,78 @@
-from tkinter import *
-from string import *
+"""Imported modules."""
+from tkinter import * ##Import the tkinter module to allow construction of the GUI interface.
+from string import * ##Module containing functions to modify strings.
 
-def create_text(agentName, agentBehaviour, circleTableValues,
-                lambdaTableValues, stimDict, bevDict, entriesCBS):
-    #Create file if it does not exist
-    file = open("agentspec.txt", "w+") 
+"""Function which creates the product (text file)."""
+def create_text(agentName, agentBehaviour, entriesCBS, circleTableValues,
+                lambdaTableValues, stimDict, bevDict):
+  """ (str, str, str, dict, dict, dict, dict) -> (none)
     
-    #text for agent
-    file.write("begin AGENT where\n")
-    # Printing agent name and behaviour
-    file.write("    %s := %s\n" %(agentName, agentBehaviour)) 
-    #END
-    file.write("end\n")
-    file.write("\n")
+    Creates the text file for the agent specifications.
+    File is saved in program's folder.
+  
+  """        
+  ##Create file if it does not exist.
+  file = open("agentspec.txt", "w+") 
     
-    #text for next behaviour
-    file.write("begin NEXT_BEHAVIOUR where\n")
-    #access each element in stimulus dictionary
-    for i in range(1, len(stimDict)+1): 
-        #access each element in behaviour dictionary
-        for j in range(1, len(bevDict)+1): 
-            #write stimulus and behaviour
-            file.write("    (%s, %s)" %(stimDict[i], bevDict[j]))
-            #finding total whitespace to align '=' sign
-            whiteSpace = len(max(stimDict.values(), key=len)) + len(max(bevDict.values(), key=len))- len(stimDict[i]) - len(bevDict[j])
-            for k in range(whiteSpace + 1):
-                file.write(" ")
-            #Access table values using row and columns
-            file.write("= %s" %(circleTableValues[j, i])) 
-            file.write("\n")
-    #END
-    file.write("end\n")
-    file.write("\n")
-    
-    #text for next stimulus
-    file.write("begin NEXT_STIMULUS where\n")
-    #access each element in stimulus dictionary
-    for i in range(1, len(stimDict)+1):
-        #access each element in behaviour dictionary
-        for j in range(1, len(bevDict)+1):
-            #write stimulus and behaviour
-            file.write("    (%s, %s)" %(stimDict[i], bevDict[j]))
-            #finding total whitespace to align '=' sign
-            whiteSpace = len(max(stimDict.values(), key=len)) + len(max(bevDict.values(), key=len)) - len(stimDict[i]) - len(bevDict[j])
-            for k in range(whiteSpace + 1):
-                file.write(" ")
-            #Access table values using row and columns
-            file.write("= %s" %(lambdaTableValues[j, i]))
-            file.write("\n")
-    #END
-    file.write("end\n")
-    file.write("\n")
-    
-    #text for concrete behaviours
-    file.write("begin CONCRETE BEHAVIOUR where\n")
-    #iterate through concrete behaviours
-    for i in range(1, len(bevDict) +1):
-        #write label for CBS
-        file.write("    %s" %(entriesCBS[i, 0].cget("text")))
-        #finding total whitespace to align '=>' sign
-        whiteSpace = len(max(bevDict.values(), key=len)) - len(entriesCBS[i, 0].cget("text"))
-        for j in range(whiteSpace + 1):
-            file.write(" ")
-        #write concrete behaviour entry for each label
-        file.write("=> [ %s ]" %(entriesCBS[i, 1].get()))
-        file.write("\n")
-    #END
-    file.write("end")    
-    file.close()
+  ##Text for the agent.
+  file.write("begin AGENT where\n")
+  ##Printing the agent name and behaviour.
+  file.write("    %s := %s\n" %(agentName, agentBehaviour)) 
+  ##END.
+  file.write("end\n")
+  file.write("\n")
+  
+  ##Text for the next behaviour.
+  file.write("begin NEXT_BEHAVIOUR where\n")
+  ##Access each element in stimulus dictionary.
+  for i in range(1, len(stimDict)+1): 
+      ##Access each element in behaviour dictionary.
+      for j in range(1, len(bevDict)+1): 
+          ##Write stimulus and behaviour.
+          file.write("    (%s, %s)" %(stimDict[i], bevDict[j]))
+          ##Finding total whitespace to align '=' sign.
+          whiteSpace = len(max(stimDict.values(), key=len)) + len(max(bevDict.values(), key=len))- len(stimDict[i]) - len(bevDict[j])
+          for k in range(whiteSpace + 1):
+              file.write(" ")
+          ##Access table values using row and columns.
+          file.write("= %s" %(circleTableValues[j, i])) 
+          file.write("\n")
+  ##END.
+  file.write("end\n")
+  file.write("\n")
+  
+  ##Text for next stimulus.
+  file.write("begin NEXT_STIMULUS where\n")
+  ##Access each element in stimulus dictionary.
+  for i in range(1, len(stimDict)+1):
+      ##Access each element in behaviour dictionary.
+      for j in range(1, len(bevDict)+1):
+          ##Write stimulus and behaviour.
+          file.write("    (%s, %s)" %(stimDict[i], bevDict[j]))
+          ##Finding total whitespace to align '=' sign.
+          whiteSpace = len(max(stimDict.values(), key=len)) + len(max(bevDict.values(), key=len)) - len(stimDict[i]) - len(bevDict[j])
+          for k in range(whiteSpace + 1):
+              file.write(" ")
+          ##Access table values using row and columns.
+          file.write("= %s" %(lambdaTableValues[j, i]))
+          file.write("\n")
+  ##END.
+  file.write("end\n")
+  file.write("\n")
+  
+  ##Text for concrete behaviours.
+  file.write("begin CONCRETE BEHAVIOUR where\n")
+  ##Iterate through concrete behaviours.
+  for i in range(1, len(bevDict) +1):
+      ##Write label for CBS.
+      file.write("    %s" %(entriesCBS[i, 0].cget("text")))
+      ##Finding total whitespace to align '=>' sign.
+      whiteSpace = len(max(bevDict.values(), key=len)) - len(entriesCBS[i, 0].cget("text"))
+      for j in range(whiteSpace + 1):
+          file.write(" ")
+      ##Write concrete behaviour entry for each label.
+      file.write("=> [ %s ]" %(entriesCBS[i, 1].get()))
+      file.write("\n")
+  ##END.
+  file.write("end")    
+  file.close()
